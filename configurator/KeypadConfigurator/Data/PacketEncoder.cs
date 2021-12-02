db3446b23bd5a50ada3a8ff2f6de85ff74e425ba
Adryzz <46694241+adryzz@users.noreply.github.com>
2021-12-03 00:24:46 +0100

    add decoding

--- configurator/KeypadConfigurator/Data/PacketEncoder.cs
+++ configurator/KeypadConfigurator/Data/PacketEncoder.cs
@@ -80,6 +80,57 @@ namespace KeypadConfigurator.Data
             if (type == PacketType.ClientHandshake)
                 return new ClientHandshakePacket();
 
+            if (type == PacketType.Configuration)
+            {
+                int index = 1;
+                KeypadConfiguration config = new KeypadConfiguration();
+
+                config.Vid = BitConverter.ToUInt16(packet, index);
+                index += sizeof(ushort);
+                
+                config.Pid = BitConverter.ToUInt16(packet, index);
+                index += sizeof(ushort);
+
+                ushort friendlyNameLength = BitConverter.ToUInt16(packet, index);
+                index += sizeof(ushort);
+
+                byte[] name = new byte[friendlyNameLength];
+                packet.CopyTo(name, index);
+                
+                config.FriendlyName = Encoding.ASCII.GetString(name);
+                index += friendlyNameLength;
+
+                KeyConfiguration c0 = new KeyConfiguration();
+                
+                c0.Pin = packet[index];
+                index += sizeof(byte);
+
+                c0.KeyChar = (char)packet[index];
+                index += sizeof(char);
+                
+                c0.DebounceTime = BitConverter.ToDouble(packet, index);
+                index += sizeof(double);
+                
+                KeyConfiguration c1 = new KeyConfiguration();
+                
+                c1.Pin = packet[index];
+                index += sizeof(byte);
+
+                c1.KeyChar = (char)packet[index];
+                index += sizeof(char);
+                
+                c1.DebounceTime = BitConverter.ToDouble(packet, index);
+                index += sizeof(double);
+
+                config.LeftKey = c0;
+                config.RightKey = c1;
+
+                return new ConfiguratonPacket
+                {
+                    Configuration = config
+                };
+            }
+
             return null;
         }
     }
