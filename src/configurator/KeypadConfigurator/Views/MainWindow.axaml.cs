6f5e383bee3a82f4426ced5d8aa9bcecd33aceaa
Adryzz <46694241+adryzz@users.noreply.github.com>
2021-12-03 23:19:17 +0100

    forgor to commit

--- src/configurator/KeypadConfigurator/Views/MainWindow.axaml.cs
+++ src/configurator/KeypadConfigurator/Views/MainWindow.axaml.cs
@@ -1,7 +1,9 @@
+using System;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using KeypadConfigurator.Data;
 
 namespace KeypadConfigurator.Views
 {
@@ -18,9 +20,46 @@ namespace KeypadConfigurator.Views
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+            var portBox = this.Find<ComboBox>("PortBox");
+            portBox.Items = KeypadConnectionManager.GetAvailablePorts();
         }
         
-        public void button_Click(object sender, RoutedEventArgs e)
+        private void applyButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Connect_OnClickButton_Click(object? sender, RoutedEventArgs e)
+        {
+            var portBox = this.Find<ComboBox>("PortBox");
+            var propsPanel = this.Find<StackPanel>("PropsPanel");
+            var connectButton = this.Find<Button>("Connect");
+            var disconnectButton = this.Find<Button>("Disconnect");
+            
+            if (KeypadConnectionManager.Connected)
+            {
+                KeypadConnectionManager.Close();
+                portBox.PlaceholderText = "Connect";
+                portBox.IsEnabled = true;
+                propsPanel.IsEnabled = false;
+                disconnectButton.IsEnabled = false;
+                connectButton.IsEnabled = true;
+            }
+            else
+            {
+                if (portBox.SelectedItem != null)
+                {
+                    KeypadConnectionManager.Initialize(portBox.SelectedItem.ToString());
+                    portBox.PlaceholderText = "Disconnect";
+                    portBox.IsEnabled = false;
+                    propsPanel.IsEnabled = true;
+                    disconnectButton.IsEnabled = true;
+                    connectButton.IsEnabled = false;
+                }
+            }
+        }
+
+        private void resetButton_Click(object? sender, RoutedEventArgs e)
         {
             
         }
