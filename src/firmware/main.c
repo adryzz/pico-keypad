be25d7aae1c86f3da2ad2182a981c60420ca225e
Adryzz <46694241+adryzz@users.noreply.github.com>
2021-12-04 10:13:16 +0100

    bedge

--- src/firmware/main.c
+++ src/firmware/main.c
@@ -1,4 +1,28 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include <string.h>
+
+#include "bsp/board.h"
+#include "tusb.h"
+#include "keyboard.h"
+
 int main(void)
 {
+    //TODO: read config
+
+    board_init(); // Initialize stuff such as led?
+    tusb_init(); // initialize the device
+
+    board_led_write(true);
+
+    prepare_GPIO();
+
+    //do stuff
+    while(1)
+    {
+        tud_task(); // tinyusb device task, handle event such as mount and unmout, used to provide the descriptor and configs
+        sense_task();
+    }
+
     return 0;
 }
